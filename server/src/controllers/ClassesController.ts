82887915f0049632a53717bbfdf557616589171b
Eduardo <eduardo_y05@outlook.com>
2020-08-04 15:24:52 -0300

    Move '/classes' route to ClassesController

---
+++ server/src/controllers/ClassesController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express';
+
+import convertHourToMinutes from "../utils/convertHourToMinutes";
+import db from "../database/connection";
+
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
+
+export default class ClassesController {
+    async create (request: Request, response: Response) {
+        const {
+            name,
+            avatar,
+            whatsapp,
+            bio,
+            subject,
+            cost,
+            schedule
+        } = request.body;
+    
+        const trx = await db.transaction();
+        
+        try {
+            const [user_id] = await trx('users').insert({
+                name,
+                avatar,
+                whatsapp,
+                bio
+            });
+    
+            const [class_id] = await trx('classes').insert({
+                subject,
+                cost,
+                user_id
+            });
+    
+            const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+                return {
+                    class_id,
+                    week_day: scheduleItem.week_day,
+                    from: convertHourToMinutes(scheduleItem.from),
+                    to: convertHourToMinutes(scheduleItem.to)
+                };
+            });
+    
+            await trx('class_schedule').insert(classSchedule);
+    
+            await trx.commit();
+    
+            return response.status(201).send();
+        } catch (err) {
+            await trx.rollback();
+    
+            return response.status(400).json({
+                error: 'Unexpected error while creating new class'
+            });
+        }
+    }
+}
